df6fb387155b2003433ea957e4a22e6222959a0a
TITAN-85 <102234554+TITAN-85@users.noreply.github.com>
2023-01-08 22:48:38 -0500

    media

--- app/controleurs/Routeur.class.php
+++ app/controleurs/Routeur.class.php
@@ -38,11 +38,11 @@ class Routeur {
   // const BASE_URI = '/Projet-web-1/';
 
   // HOME
-  // const BASE_URI = '/git/voiture/'; 
+  const BASE_URI = '/git/voiture/'; 
 
   //.ionos.com
   // const BASE_URI = '/kunden/homepages/41/d947040641/htdocs/';
-  const BASE_URI = '/accueil/'; 
+  // const BASE_URI = '/'; 
 
   
   const ERROR_FORBIDDEN = "HTTP 403";
